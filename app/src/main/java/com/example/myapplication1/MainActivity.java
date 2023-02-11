f3b6bf1b1b1d1757249ce1dd075c3811623adebc
Andre Kent Ricablanca <mavenking123@gmail.com>
2023-02-11 16:44:49 +0800

    Initial MyApplication Project

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose tag.");
+        Log.d(TAG, "This is a debug log.");
+        Log.i(TAG,"This is an info log.");
+        Log.w(TAG, "This is a warn log");
+        Log.e(TAG, "This is an error log.");
+
+
+
     }
+
+
 }
