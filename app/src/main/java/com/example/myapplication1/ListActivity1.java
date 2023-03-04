879231d6df1c36b5ff9d468b8006d976ff07f4a9
Andre Kent Ricablanca <mavenking123@gmail.com>
2023-03-04 17:38:54 +0800

    List View Activity 2, March 4

---
+++ app/src/main/java/com/example/myapplication1/ListActivity1.java
@@ -0,0 +1,34 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.*;
+
+public class ListActivity1 extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+    String[] listItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list1);
+
+        listView= (ListView) findViewById(R.id.listView);
+        listItem=getResources().getStringArray(R.array.planets_array);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.my_list, R.id.textView, listItem);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String value = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
