1478128ea529d6c5e6b1dbc424a6f99c1f3bee31
Andre Kent Ricablanca <mavenking123@gmail.com>
2023-03-04 16:55:26 +0800

    Inputs

---
+++ app/src/main/java/com/example/myapplication1/ListActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
+
+import java.util.*;
+
+public class ListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+    }
+}
