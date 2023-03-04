3286733d1cc660a4132394676ec2e7b4aea590f9
Andre Kent Ricablanca <mavenking123@gmail.com>
2023-03-04 17:06:01 +0800

    Inputs

--- app/src/main/java/com/example/myapplication1/ListActivity.java
+++ app/src/main/java/com/example/myapplication1/ListActivity.java
@@ -9,11 +9,36 @@ import android.view.View;
 import java.util.*;
 
 public class ListActivity extends AppCompatActivity {
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
+        spinner = findViewById(R.id.spinner1);
+        List<String> footballPlayers = new ArrayList<>();
+        footballPlayers.add(0, "Select a player from the list");
+        footballPlayers.add("Cristian Ronaldo");
+        footballPlayers.add("Lionel Messi");
+        footballPlayers.add("Neymar Jr");
+        footballPlayers.add("Isco");
+        footballPlayers.add("Gareth Bale");
+        footballPlayers.add("Luis Suarez");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, footballPlayers);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getItemAtPosition(position).equals("Choose Football players from lis")){
+                }else {
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
     }
 }
