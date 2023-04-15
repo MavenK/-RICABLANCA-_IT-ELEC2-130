f173d12d075133369eb5c1b5d0ff748e25d1f5c0
Andre Kent Ricablanca <mavenking123@gmail.com>
2023-04-15 16:13:05 +0800

    April 15 Intent Activity

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -11,31 +11,11 @@ import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Today's Activity is an introduction of logs. This is by Andre Kent Ricablanca.
-        Log.v(TAG, "This is a verbose log.");
-        Log.d(TAG, "This is a debug log.");
-        Log.i(TAG,"This is an info log.");
-        Log.w(TAG, "This is a warn log");
-        Log.e(TAG, "This is an error log.");
-
-        Button button = (Button) findViewById(R.id.button1);
-
-        button.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v){
-                Log.i(TAG, "Button Clicked");
-
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
 
 
     }
