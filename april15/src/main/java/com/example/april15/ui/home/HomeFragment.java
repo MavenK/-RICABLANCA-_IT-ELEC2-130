8df050058febfc6b27699fff7f592b0258d0648d
Andre Kent Ricablanca <mavenking123@gmail.com>
2023-04-15 17:05:51 +0800

    April 15 Intent Activity and Bottom Navigation

--- april15/src/main/java/com/example/april15/ui/home/HomeFragment.java
+++ april15/src/main/java/com/example/april15/ui/home/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.april15.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.april15.SecondActivity;
 import com.example.april15.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -26,6 +29,16 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        final Button buttonGoToSecond = binding.buttonGotosecond;
+        buttonGoToSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SecondActivity.class);
+                intent.putExtra("PASS_ME", "Hi I'm from Main Activity!!!");
+                startActivity(intent);
+            }
+        });
         return root;
     }
 
